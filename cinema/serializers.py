b9f5785a00e6b3539399368fdb604083b1f9898a
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-07-26 13:35:34 +0300

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,24 +1,49 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
-from cinema.models import Movie
+from cinema.models import (
+    Actor,
+    CinemaHall,
+    Genre,
+    Movie,
+)
 
 
-class MovieSerializer(serializers.Serializer):
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    actors = ActorSerializer(read_only=True, many=True)
+    genres = GenreSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Movie
+        fields = "__all__"
+
+
+class CinemaHallSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    description = serializers.CharField()
-    duration = serializers.IntegerField()
+    rows = serializers.IntegerField()
+    seats_in_row = serializers.IntegerField()
 
     def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+        return CinemaHall.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.title = validated_data.get("title", instance.title)
-        instance.description = validated_data.get(
-            "description", instance.description
+        instance.rows = validated_data.get("rows", instance.rows)
+        instance.seats_in_row = validated_data.get(
+            "seats_in_row",
+            instance.seats_in_row
         )
-        instance.duration = validated_data.get("duration", instance.duration)
-
         instance.save()
 
         return instance
