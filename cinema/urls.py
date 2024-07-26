97d557a8433728313d28b62a224fa5a651b0a050
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-07-26 13:38:03 +0300

    fix flake8 error

--- cinema/urls.py
+++ cinema/urls.py
@@ -36,7 +36,11 @@ urlpatterns = [
     path("actors/", ActorListView.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetailView.as_view(), name="actor-detail"),
     path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
-    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
+    path(
+        "cinema_halls/<int:pk>/",
+        cinema_hall_detail,
+        name="cinema-hall-detail"
+    ),
     path("", include(movie_router.urls)),
 ]
 
